6df238595fff007cdab06f5ae7accb820b952716
Elmo Pair <elpair@gmail.com>
2020-06-16 10:20:20 -0400

    moves script logic into separate files

---
+++ setup-brew.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install Apps
+brew cask install google-chrome
+brew cask install spotify
+brew cask install hyper
+brew cask install 1password
+brew cask install atom
+brew cask install hammerspoon
+
+# Makes chrome default browser
+brew install defaultbrowser
+defaultbrowser chrome
