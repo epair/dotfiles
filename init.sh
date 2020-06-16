691d1b408878793a2fb4ef0ae9fa5e755dd75525
Elmo Pair <elpair@gmail.com>
2020-06-16 13:40:42 -0400

    add setup-dock and move symlinks

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-chmod +x ~/code/dotfiles/setup-brew.sh ~/code/dotfiles/setup-atom.sh ~/code/dotfiles/setup-github.sh ~/code/dotfiles/setup-macos.sh ~/code/dotfiles/setup-symlinks.sh ~/code/dotfiles/setup-zsh.sh
+chmod +x ~/code/dotfiles/setup-brew.sh ~/code/dotfiles/setup-atom.sh ~/code/dotfiles/setup-github.sh ~/code/dotfiles/setup-macos.sh ~/code/dotfiles/setup-symlinks.sh ~/code/dotfiles/setup-zsh.sh  ~/code/dotfiles/setup-dock.sh
 
 source ~/code/dotfiles/setup-brew.sh
 source ~/code/dotfiles/setup-macos.sh
-source ~/code/dotfiles/setup-atom.sh
+source ~/code/dotfiles/setup-dock.sh
 source ~/code/dotfiles/setup-symlinks.sh
+source ~/code/dotfiles/setup-atom.sh
 source ~/code/dotfiles/setup-zsh.sh
 
 echo "Setup complete. Switching to zsh..."
