f437a5bf2307fde598760f2c43bbe2e232531792
Elmo Pair <elpair@gmail.com>
2020-06-16 10:34:14 -0400

    add completed notification

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -5,3 +5,5 @@
 ln -s ~/code/dotfiles/zshrc ~/.zshrc
 mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/keymap.cson ~/.atom/keymap.cson
+
+echo "Symlink setup complete."
