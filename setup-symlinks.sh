e125699d98e6db9e39de508c767e0903156d5eca
Elmo Pair <elpair@gmail.com>
2020-06-16 16:14:31 -0400

    update setup-symlinks.sh

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -2,7 +2,7 @@
 
 # Setup Symlinks to dotfiles
 
-ln -s ~/code/dotfiles/zshrc ~/.zshrc
+rm ~/.zshrc && ln -s ~/code/dotfiles/zshrc ~/.zshrc
 mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/keymap.cson ~/.atom/keymap.cson
 
