71d798943e7c681640dd467fd4372f2f4b090e1c
Elmo Pair <elpair@gmail.com>
2020-06-16 10:32:05 -0400

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chmod +x ~/code/dotfiles/setup-brew.sh ~/code/dotfiles/setup-atom.sh ~/code/dotfiles/setup-github.sh ~/code/dotfiles/setup-macos.sh ~/code/dotfiles/setup-symlinks.sh ~/code/dotfiles/setup-zsh.sh
+
+source ~/code/dotfiles/setup-brew.sh
+source ~/code/dotfiles/setup-macos.sh
+source ~/code/dotfiles/setup-atom.sh
+source ~/code/dotfiles/setup-zsh.sh
+source ~/code/dotfiles/setup-symlinks.sh
+
+echo "Setup complete. Switching to zsh..."
+chsh -s /usr/local/bin/zsh
