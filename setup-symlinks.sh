53bad4925add7946876fa8f5bcaab164ef60da1f
Elmo Pair <elpair@gmail.com>
2020-06-17 10:42:18 -0400

    remove keymap.cson default file

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,6 +4,7 @@
 
 rm ~/.zshrc && ln -s ~/code/dotfiles/zshrc ~/.zshrc
 mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
+rm ~/.atom/keymap.cson
 ln -s ~/code/dotfiles/keymap.cson ~/.atom/keymap.cson
 
 echo "Symlink setup complete."
