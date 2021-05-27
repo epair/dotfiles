9f6a901344f54285435e55f924574aa8eead3ba0
Emmett Pair <elpair@gmail.com>
2021-05-26 23:43:53 -0400

    adds vimrc and symlink

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,5 +6,6 @@ rm ~/.zshrc && ln -s ~/code/dotfiles/zshrc ~/.zshrc
 mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
 rm ~/.atom/keymap.cson
 ln -s ~/code/dotfiles/keymap.cson ~/.atom/keymap.cson
+ln -s ~/code/dotfiles/vim/vimrc ~/.vimrc
 
 echo "Symlink setup complete."
