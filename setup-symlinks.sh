b8b03d6c60f4ca23e2ce555f2caecde284f46d3b
Emmett Pair <elpair@gmail.com>
2021-08-16 20:31:37 -0400

    Add gitconfig to dotfiles & symlink setup

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -7,5 +7,7 @@ mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/vim/vimrc ~/.vimrc
 rm ~/.config/nvim/init.vim
 ln -s ~/code/dotfiles/nvim ~/.config/nvim/init.vim
+rm ~/.gitconfig
+ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
 
 echo "Symlink setup complete."
