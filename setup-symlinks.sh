92a9b88c991b060956e7e7b06b477a0fa742e8e6
Emmett Pair <elpair@gmail.com>
2022-04-03 13:54:52 -0400

    Fix symlink script

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -5,7 +5,7 @@
 rm ~/.zshrc && ln -s ~/code/dotfiles/zshrc ~/.zshrc
 mkdir ~/.hammerspoon && ln -s ~/code/dotfiles/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/vim/vimrc ~/.vimrc
-rm ~/.config/nvim/init.vim
+mkdir ~/.config/nvim
 ln -s ~/code/dotfiles/nvim ~/.config/nvim/init.vim
 rm ~/.gitconfig
 ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
