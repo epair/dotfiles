113ca2be08dc63da998f4c179fb18466592c21de
Emmett Pair <emmett.pair@postie.com>
2023-05-20 14:17:51 -0400

    Clarify dependencies and delete unused

--- init.lua
+++ init.lua
@@ -2,53 +2,13 @@ hs.loadSpoon("SpoonInstall")
 
 spoon.SpoonInstall:andUse("AppLauncher", {
   hotkeys = {
-    p = "1Password 7",
-    z = "Zoom.us",
     c = "Google Chrome",
     t = "iTerm",
     s = "Slack",
     m = "Spotify",
+    o = "Obsidian"
   }
 })
-
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
-end)
 
 caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
---
+++ test.md
@@ -0,0 +1,4 @@
+11/30/22 10:13:10 PM - this is some text 
+
+---
+test
