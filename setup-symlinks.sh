a91d192d64b44d6ce22a3010003c1509c51f1f32
Emmett Pair <emmett.pair@postie.com>
2024-10-26 15:26:58 -0400

    Add aider config

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -8,5 +8,6 @@ ln -s ~/code/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
 ln -s ~/code/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/code/dotfiles/git/gitignore ~/.config/git/ignore
 ln -s ~/code/dotfiles/nvchad ~/.config/nvim/lua
+ln -s ~/code/dotfiles/aider.conf.yml ~/.aider.conf.yml
 
 echo "Symlink setup complete."
