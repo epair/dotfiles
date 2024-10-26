36b786a9a50eb514bc85e29db461c48c1264e52b
Emmett Pair <emmett.pair@postie.com>
2024-10-26 15:20:48 -0400

    Remove markview plugin

--- nvchad/plugins/init.lua
+++ nvchad/plugins/init.lua
@@ -56,15 +56,6 @@ return {
     },
   },
   {
-    "OXY2DEV/markview.nvim",
-    lazy = false,      -- Recommended
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons"
-    }
-  },
-
-  {
     "roobert/tailwindcss-colorizer-cmp.nvim",
     lazy = false,
     config = function()
