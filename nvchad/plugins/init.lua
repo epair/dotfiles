a0592bca49a3aeb37c02f5bcb71c1aaee0eef406
Emmett Pair <emmett.pair@postie.com>
2024-10-26 00:02:36 -0400

    Load gh telescope extension

--- nvchad/plugins/init.lua
+++ nvchad/plugins/init.lua
@@ -43,4 +43,15 @@ return {
       },
     },
   },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      require('telescope').load_extension('gh')
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-github.nvim" },
+    },
+  },
 }
