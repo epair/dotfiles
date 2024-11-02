4dc86ea3503f02aaf8eb1fdd5b308c95eb94bdb0
Emmett Pair <emmett.pair@postie.com>
2024-11-02 00:15:53 -0400

    Use official powerline font

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,11 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Solarized Dark Higher Contrast'
+config.color_scheme = "Solarized Dark Higher Contrast"
 
 config.font_size = 22
 
-config.font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font = wezterm.font("MesloGS NF")
 
 -- and finally, return the configuration to wezterm
 return config
