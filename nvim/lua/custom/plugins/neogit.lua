73b7ba876785d7bf64b428fba8a9b791c8ba8008
Emmett Pair <emmett.pair@postie.com>
2024-11-02 23:17:08 -0400

    Add custom keymap for neogit toggle

--- nvim/lua/custom/plugins/neogit.lua
+++ nvim/lua/custom/plugins/neogit.lua
@@ -6,6 +6,14 @@ return {
       'sindrets/diffview.nvim', -- optional - Diff integration
       'nvim-telescope/telescope.nvim', -- optional
     },
-    config = true,
+    config = function()
+      require('neogit').setup {
+        mappings = {
+          status = {
+            ['='] = 'Toggle',
+          },
+        },
+      }
+    end,
   },
 }
