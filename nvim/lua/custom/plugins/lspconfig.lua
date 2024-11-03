26b0acca7b6bb16420091812340590dee9f4d851
Emmett Pair <emmett.pair@postie.com>
2024-11-03 11:09:37 -0500

    Remove config from attempt at using solarized

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -210,19 +210,6 @@ return {
               completion = {
                 callSnippet = 'Replace',
               },
-              hint = {
-                enable = true,
-              },
-              runtime = {
-                version = 'LuaJIT',
-              },
-              workspace = {
-                checkThirdParty = true,
-                library = {
-                  vim.env.VIMRUNTIME,
-                  '~/.local/share/nvim/lazy/solarized.nvim',
-                },
-              },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
