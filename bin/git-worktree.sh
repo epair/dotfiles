cd28b308f25bc0535f3d9bbcb7b8abd5f7d97d95
Emmett Pair <emmett.pair@postie.com>
2024-11-06 02:05:36 -0500

    Add remove command to remove worktree

--- bin/git-worktree.sh
+++ bin/git-worktree.sh
@@ -3,16 +3,18 @@
 PROJECT_NAME="letterpress-app"
 PARENT_DIR=$(dirname "$PWD")
 PARENT_DIR_BASENAME=$(basename "$PARENT_DIR")
-BRANCH_NAME="ep/$1"
-WORKTREE_NAME=$1
-DIR="$PARENT_DIR/.gitworktrees/$WORKTREE_NAME"
 
-# Check if branch name argument is provided
-if [ $# -eq 0 ]; then
-    echo "Error: Branch name argument is required"
-    echo "Usage: $0 <branch-name>"
+# Check if command and branch name arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Error: Both command and branch name arguments are required"
+    echo "Usage: $0 <add|remove> <branch-name>"
     exit 1
 fi
+
+COMMAND=$1
+WORKTREE_NAME=$2
+BRANCH_NAME="ep/$WORKTREE_NAME"
+DIR="$PARENT_DIR/.gitworktrees/$WORKTREE_NAME"
 
 # Exit if not in the correct project directory
 if [[ $PARENT_DIR_BASENAME != "$PROJECT_NAME" ]]; then
@@ -20,32 +22,45 @@ if [[ $PARENT_DIR_BASENAME != "$PROJECT_NAME" ]]; then
     exit 1
 fi
 
-# Create new branch
-echo "Creating branch $BRANCH_NAME"
-git branch "$BRANCH_NAME"
+if [ "$COMMAND" = "add" ]; then
+    # Create new branch
+    echo "Creating branch $BRANCH_NAME"
+    git branch "$BRANCH_NAME"
 
-# Create .gitworktrees directory if it doesn't exist
-mkdir -p "$PARENT_DIR/.gitworktrees"
+    # Create .gitworktrees directory if it doesn't exist
+    mkdir -p "$PARENT_DIR/.gitworktrees"
 
-# Add worktree for the new branch
-echo "Creating worktree $DIR"
-git worktree add "$DIR" "$BRANCH_NAME"
+    # Add worktree for the new branch
+    echo "Creating worktree $DIR"
+    git worktree add "$DIR" "$BRANCH_NAME"
 
-# Output command to change directory
-echo "Changing directory to $DIR"
-cd $DIR
+    # Output command to change directory
+    echo "Changing directory to $DIR"
+    cd $DIR
 
-echo "Setting up worktree $WORKTREE_NAME"
+    echo "Setting up worktree $WORKTREE_NAME"
 
-# Create .env file with COMPOSE_PROJECT_NAME
-echo "COMPOSE_PROJECT_NAME=$PROJECT_NAME" > .env
+    # Create .env file with COMPOSE_PROJECT_NAME
+    echo "COMPOSE_PROJECT_NAME=$PROJECT_NAME" > .env
 
-# Create config directory and copy application.yml from parent directory
-cp "$PARENT_DIR/config/application.yml" "./config/."
+    # Create config directory and copy application.yml from parent directory
+    cp "$PARENT_DIR/config/application.yml" "./config/."
 
-mkdir -p tmp/prs
-touch "./tmp/prs/$WORKTREE_NAME.md"
+    mkdir -p tmp/prs
+    touch "./tmp/prs/$WORKTREE_NAME.md"
 
-zoxide add "$PWD"
+    zoxide add "$PWD"
 
-echo "Done."
+    echo "Done."
+
+elif [ "$COMMAND" = "remove" ]; then
+    echo "Removing worktree and branch $BRANCH_NAME"
+    cd $PARENT_DIR
+    git worktree remove $WORKTREE_NAME
+    git branch -D $BRANCH_NAME
+    zoxide remove "$DIR"
+    echo "Done."
+else
+    echo "Error: Invalid command. Use 'add' or 'remove'"
+    exit 1
+fi
