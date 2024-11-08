f0852b4087e7149cc02c95515f5d96910330327a
Emmett Pair <emmett.pair@postie.com>
2024-11-07 22:59:51 -0500

    Finalize wtree script by converting to zsh

--- bin/git-worktree.sh
+++ bin/git-worktree.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/zsh
 
 PROJECT_NAME="letterpress-app"
-PARENT_DIR=$(dirname "$PWD")
-PARENT_DIR_BASENAME=$(basename "$PARENT_DIR")
+PARENT_DIR="$HOME/code/postie/letterpress-app"
 
 # Check if command and branch name arguments are provided
-if [ $# -ne 2 ]; then
+if (( $# != 2 )); then
     echo "Error: Both command and branch name arguments are required"
     echo "Usage: $0 <add|remove> <branch-name>"
     exit 1
@@ -16,13 +15,7 @@ WORKTREE_NAME=$2
 BRANCH_NAME="ep/$WORKTREE_NAME"
 DIR="$PARENT_DIR/.gitworktrees/$WORKTREE_NAME"
 
-# Exit if not in the correct project directory
-if [[ $PARENT_DIR_BASENAME != "$PROJECT_NAME" ]]; then
-    echo "Error: This script must be run from within the $PROJECT_NAME project"
-    exit 1
-fi
-
-if [ "$COMMAND" = "add" ]; then
+if [[ "$COMMAND" == "add" ]]; then
     # Create new branch
     echo "Creating branch $BRANCH_NAME"
     git branch "$BRANCH_NAME"
@@ -34,27 +27,23 @@ if [ "$COMMAND" = "add" ]; then
     echo "Creating worktree $DIR"
     git worktree add "$DIR" "$BRANCH_NAME"
 
-    # Output command to change directory
-    echo "Changing directory to $DIR"
-    cd $DIR
-
     echo "Setting up worktree $WORKTREE_NAME"
 
     # Create .env file with COMPOSE_PROJECT_NAME
-    echo "COMPOSE_PROJECT_NAME=$PROJECT_NAME" > .env
+    echo "COMPOSE_PROJECT_NAME=$PROJECT_NAME" > "$DIR/.env"
 
     # Create config directory and copy application.yml from parent directory
-    cp "$PARENT_DIR/config/application.yml" "./config/."
+    cp "$PARENT_DIR/config/application.yml" "$DIR/config/."
 
-    mkdir -p tmp/prs
-    touch "./tmp/prs/$WORKTREE_NAME.md"
+    mkdir -p "$DIR/tmp/prs"
+    touch "$DIR/tmp/prs/$WORKTREE_NAME.md"
 
-    zoxide add "$PWD"
+    echo "Adding worktree to zoxide"
+    zoxide add "$DIR"
+
+    echo "Connecting to tmux session $WORKTREE_NAME..."
     sesh connect $WORKTREE_NAME
-
-    echo "Done."
-
-elif [ "$COMMAND" = "remove" ]; then
+elif [[ "$COMMAND" == "remove" ]]; then
     echo "Removing worktree and branch $BRANCH_NAME"
 
     # Only cd to parent directory if we're currently in the worktree we're removing
@@ -64,7 +53,6 @@ elif [ "$COMMAND" = "remove" ]; then
 
     git worktree remove $WORKTREE_NAME
     git branch -D $BRANCH_NAME
-    zoxide remove "$DIR"
     tmux kill-session -t $WORKTREE_NAME
     echo "Done."
 else
