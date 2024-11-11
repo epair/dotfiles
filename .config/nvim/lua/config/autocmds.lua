ca8e96ec3474e02d505efd3deb22456fe678084f
Emmett Pair <emmett.pair@postie.com>
2024-11-11 17:09:10 -0500

    Add autocmds

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,13 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*tmux.conf' },
+  command = "execute 'silent !tmux source <afile> --silent'",
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { 'aerospace.toml' },
+  command = '!aerospace reload-config',
+})
