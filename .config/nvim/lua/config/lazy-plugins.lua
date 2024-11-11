f4290502a8b8dbce0fb2b61e0c83b1136c18dfae
Emmett Pair <emmett.pair@postie.com>
2024-11-11 17:03:45 -0500

    Simply file structure for nvim config

--- .config/nvim/lua/config/lazy-plugins.lua
+++ .config/nvim/lua/config/lazy-plugins.lua
@@ -17,7 +17,7 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  { import = 'plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
