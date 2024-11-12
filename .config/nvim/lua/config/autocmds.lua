e1b20631446d93d85af033387e882a7b49ec1314
Emmett Pair <emmett.pair@postie.com>
2024-11-11 23:59:53 -0500

    Move all nvim autocmds into config file

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,5 @@
+-- [[ Basic Autocommands ]]
+-- See `:help lua-guide-autocommands`
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
@@ -10,4 +12,27 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = { 'aerospace.toml' },
   command = '!aerospace reload-config',
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '.zshrc' },
+  command = "source ~/.zshrc",
+})
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Disable line numbers in terminal mode
+vim.api.nvim_create_autocmd('TermOpen', {
+  callback = function()
+    vim.opt_local.number = false
+  end,
 })
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -47,24 +47,4 @@ set('n', '<M-j>', '<C-w>-')
 set('n', '<M-k>', '<C-w>+')
 set('n', '<M-e>', '<C-w>=')
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- Disable line numbers in terminal mode
-vim.api.nvim_create_autocmd('TermOpen', {
-  callback = function()
-    vim.opt_local.number = false
-  end,
-})
 -- vim: ts=2 sts=2 sw=2 et
