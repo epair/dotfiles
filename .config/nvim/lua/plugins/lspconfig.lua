704d9b9e8089f221f880a1492058f3c32f17bfb5
Emmett Pair <emmett.pair@postie.com>
2024-11-15 23:53:14 -0500

    Customize diagnostics text

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -138,6 +138,128 @@ return {
             })
           end
 
+          -- Only show the most critical diagnostic on the current line (keep underline and icons in gutter)
+          -- from alisnic/.dotfiles: https://github.com/alisnic/.dotfiles/blob/e5cd3f0c95132eadf0f8be09c3ce6f585ac9e921/nvim/lua/lsp.lua
+          local function best_diagnostic(diagnostics)
+            if vim.tbl_isempty(diagnostics) then
+              return
+            end
+
+            local best = nil
+            local line_diagnostics = {}
+            local line_nr = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+            for k, v in pairs(diagnostics) do
+              if v.lnum == line_nr then
+                line_diagnostics[k] = v
+              end
+            end
+
+            for _, diagnostic in ipairs(line_diagnostics) do
+              if best == nil then
+                best = diagnostic
+              elseif diagnostic.severity < best.severity then
+                best = diagnostic
+              end
+            end
+
+            return best
+          end
+
+          local function current_line_diagnostics()
+            local bufnr = 0
+            local line_nr = vim.api.nvim_win_get_cursor(0)[1] - 1
+            local opts = { ["lnum"] = line_nr }
+
+            return vim.diagnostic.get(bufnr, opts)
+          end
+
+          local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+
+          local virt_handler = vim.diagnostic.handlers.virtual_text
+          local ns = vim.api.nvim_create_namespace "current_line_virt"
+          local severity = vim.diagnostic.severity
+          local virt_options = {
+            prefix = "",
+            format = function(diagnostic)
+              local message = vim.split(diagnostic.message, "\n")[1]
+
+              if diagnostic.severity == severity.ERROR then
+                return signs.Error .. " " .. message
+              elseif diagnostic.severity == severity.INFO then
+                return signs.Info .. " " .. message
+              elseif diagnostic.severity == severity.WARN then
+                return signs.Warn .. " " .. message
+              elseif diagnostic.severity == severity.HINT then
+                return signs.Hint .. " " .. message
+              else
+                return message
+              end
+            end,
+          }
+
+          vim.diagnostic.handlers.current_line_virt = {
+            show = function(_, bufnr, diagnostics, _)
+              local diagnostic = best_diagnostic(diagnostics)
+              if not diagnostic then
+                return
+              end
+
+              local filtered_diagnostics = { diagnostic }
+
+              pcall(
+                virt_handler.show,
+                ns,
+                bufnr,
+                filtered_diagnostics,
+                { virtual_text = virt_options }
+              )
+            end,
+            hide = function(_, bufnr)
+              bufnr = bufnr or vim.api.nvim_get_current_buf()
+              virt_handler.hide(ns, bufnr)
+            end,
+          }
+
+          vim.diagnostic.config {
+            float = { source = "always" },
+            signs = true,
+            virtual_text = false,
+            severity_sort = true,
+            current_line_virt = true,
+          }
+
+          vim.api.nvim_create_augroup("lsp_diagnostic_current_line", {
+            clear = true,
+          })
+
+          vim.api.nvim_clear_autocmds {
+            buffer = event.buf,
+            group = "lsp_diagnostic_current_line",
+          }
+
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            group = "lsp_diagnostic_current_line",
+            buffer = event.buf,
+            callback = function()
+              vim.diagnostic.handlers.current_line_virt.show(
+                nil,
+                0,
+                current_line_diagnostics(),
+                nil
+              )
+            end,
+          })
+
+          vim.api.nvim_create_autocmd("CursorMoved", {
+            group = "lsp_diagnostic_current_line",
+            buffer = event.buf,
+            callback = function()
+              vim.diagnostic.handlers.current_line_virt.hide(nil, nil)
+            end,
+          })
+          -- end config for only showing most critical diagnostic on current line
+
           -- The following code creates a keymap to toggle inlay hints in your
           -- code, if the language server you are using supports them
           --
@@ -151,13 +273,13 @@ return {
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-      --   for type, icon in pairs(signs) do
-      --     local hl = 'DiagnosticSign' .. type
-      --     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      --   end
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+        for type, icon in pairs(signs) do
+          local hl = 'DiagnosticSign' .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
