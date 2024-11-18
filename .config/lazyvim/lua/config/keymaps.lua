466a69c528dbebd4a1594b7468f57babd7bbf7ff
Emmett Pair <emmett.pair@postie.com>
2024-11-17 22:45:18 -0500

    Add lazyvim

---
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local set = vim.keymap.set
+
+set("i", "jk", "<Esc>")
+set("i", "kj", "<Esc>")
