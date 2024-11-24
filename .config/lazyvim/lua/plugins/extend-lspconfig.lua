57cbcad1bcdf51a68414aee614b64f618c17285e
Emmett Pair <emmett.pair@postie.com>
2024-11-23 21:19:14 -0500

    Ensure ruby-lsp runs by using global gemfile

--- .config/lazyvim/lua/plugins/extend-lspconfig.lua
+++ .config/lazyvim/lua/plugins/extend-lspconfig.lua
@@ -3,6 +3,11 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+      servers = {
+        ruby_lsp = {
+          cmd_env = { BUNDLE_GEMFILE = vim.fn.getenv("GLOBAL_GEMFILE") },
+        },
+      },
       diagnostics = {
         virtual_text = false,
       },
