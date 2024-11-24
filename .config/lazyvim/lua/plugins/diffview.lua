f4915a099b8fa5c79bd8eab99eb1a21a05499923
Emmett Pair <emmett.pair@postie.com>
2024-11-23 21:15:08 -0500

    Add diffview plugin

---
+++ .config/lazyvim/lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = "DiffviewOpen",
+  opts = {},
+  keys = {
+
+    { "<leader>gD", "<cmd>DiffviewOpen<CR>", desc = "Open Diffview" },
+    { "<leader>gC", "<cmd>DiffviewClose<CR>", desc = "Close Diffview" },
+  },
+}
