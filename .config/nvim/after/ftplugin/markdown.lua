fb87c82e5eb5858abc82f61b13c5a77dd376d636
Emmett Pair <emmett.pair@postie.com>
2024-12-02 00:44:27 -0500

    Move lazyvim to default nvim dir

--- .config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.conceallevel = 1
-
--- Enable auto-wrap
-vim.opt.wrap = true
-
-vim.keymap.set('n', '<Leader>p', '<CMD>MarkdownPreview<CR>', { noremap = true, silent = true })
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,51 +1,7 @@
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
 local set = vim.keymap.set
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-set('i', 'jk', '<Esc>')
-set('i', 'kj', '<Esc>')
-
-set('n', '<leader>g', '<CMD>Neogit<CR>', { desc = 'Open Neogit' })
-
-set('n', '<leader>x', '<CMD>bd<CR>', { desc = '[C]lose current buffer' })
-set('n', '<leader>q', '<CMD>q<CR>', { desc = '[Q]uit and close nvim' })
-set('n', '<C-s>', '<CMD>w<CR>', { desc = '[S]ave current buffer' })
-
--- These mappings control the size of splits (height/width)
-set('n', '<M-,>', '<C-w>5<')
-set('n', '<M-.>', '<C-w>5>')
-set('n', '<M-j>', '<C-w>-')
-set('n', '<M-k>', '<C-w>+')
-set('n', '<M-e>', '<C-w>=')
-
--- vim: ts=2 sts=2 sw=2 et
+set("i", "jk", "<Esc>")
+set("i", "kj", "<Esc>")
