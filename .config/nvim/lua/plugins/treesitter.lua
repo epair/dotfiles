5592c4d0512353c655810a4e53e9e2e71aa21741
Emmett Pair <emmett.pair@postie.com>
2025-03-26 23:03:54 -0400

    Add treesitter-textobjects plugin for swapping params

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,46 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	main = 'nvim-treesitter.configs',
-	opts = {
-		ensure_installed = { 'bash', 'diff', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'ruby', 'vim', 'vimdoc' },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = { 'ruby' },
-		},
-		indent = { enable = true, disable = { 'ruby' } },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-n>",
-        node_incremental = "<C-n>",
-        scope_incremental = false,
-        node_decremental = "<C-p>",
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects'},
+    opts = {
+      ensure_installed = { 'bash', 'diff', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'ruby', 'vim', 'vimdoc' },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
       },
+      indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-n>",
+          node_incremental = "<C-n>",
+          scope_incremental = false,
+          node_decremental = "<C-p>",
+        },
+      }
     }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        textobjects = {
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
+        },
+      }
+    end
   }
 }
+
