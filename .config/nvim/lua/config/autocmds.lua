a202b9c02aedbd0654b896a7942b2d324ae4e9d8
Emmett Pair <emmett.pair@postie.com>
2025-07-09 22:51:02 -0400

    Save neovim buffers to sessions on change

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -17,6 +17,34 @@ local function get_session_file()
   return session_dir .. "/" .. project_name .. ".vim"
 end
 
+-- Debounced session saving
+local save_timer = nil
+local function debounced_save_session()
+  if save_timer then
+    vim.fn.timer_stop(save_timer)
+  end
+  save_timer = vim.fn.timer_start(1000, function()
+    -- Only save if we have at least one normal buffer
+    local has_normal_buffer = false
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+        local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+        local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
+        if buftype == "" and filetype ~= "netrw" then
+          has_normal_buffer = true
+          break
+        end
+      end
+    end
+
+    if has_normal_buffer then
+      local session_file = get_session_file()
+      vim.cmd("mksession! " .. session_file)
+    end
+    save_timer = nil
+  end)
+end
+
 local function augroup(name)
   return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
 end
@@ -43,6 +71,37 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+-- Save session when buffers are added
+vim.api.nvim_create_autocmd("BufAdd", {
+  group = session_augroup,
+  callback = function(args)
+    local buftype = vim.api.nvim_buf_get_option(args.buf, "buftype")
+    if buftype == "" then
+      debounced_save_session()
+    end
+  end,
+})
+
+-- Save session when buffers are deleted
+vim.api.nvim_create_autocmd("BufDelete", {
+  group = session_augroup,
+  callback = function()
+    debounced_save_session()
+  end,
+})
+
+-- Save session when buffers are written
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = session_augroup,
+  callback = function(args)
+    local buftype = vim.api.nvim_buf_get_option(args.buf, "buftype")
+    if buftype == "" then
+      debounced_save_session()
+    end
+  end,
+})
+
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2566
 vim.api.nvim_create_autocmd("FileType", {
@@ -108,11 +167,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Enable line wrapping for markdown files
+-- Enable line wrapping for markdown files and set 2-space indentation
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("markdown_wrap"),
   pattern = { "markdown" },
   callback = function()
     vim.opt_local.wrap = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
   end,
 })
