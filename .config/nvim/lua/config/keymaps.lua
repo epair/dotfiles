d0dd80c590795b5679a2b8d9c61965e843261853
Emmett Pair <emmett.pair@postie.com>
2026-04-19 23:02:01 -0400

    Map Ctrl-q to quit in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
 
 vim.keymap.set({"n", "v", "i"}, "<C-s>", ":w <CR>", { desc = 'Save' })
+vim.keymap.set({"n", "v", "i"}, "<C-q>", ":q! <CR>", { desc = 'Quit' })
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move block up' })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move block down' })
